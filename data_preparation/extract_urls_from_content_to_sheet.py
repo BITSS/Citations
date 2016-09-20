abb2e5e535b40ad60ffc516881222c4fdb70283a
djbirke <djbirke@berkeley.edu>
2016-09-20 16:05:22 -0700

    Use href to extract URLs

--- data_preparation/extract_urls_from_content_to_sheet.py
+++ data_preparation/extract_urls_from_content_to_sheet.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 '''
-Extract list of urls used in each article content to excel sheet.
+Extract list of urls used in each article content to spreadsheet.
 '''
 
-# TODO Add non-url search parameters.
-# TODO Make sure to exclude email addresses from url match.
+# TODO URL finding via href
+# TODO Fix regex sentence exclusion.
+# re.compile(r'\s\w+\.([A-Z][a-z]|[a-z][A-Z])')
+# -> re.compile(r'\s\w+\.([A-Z][a-z]|[a-z][A-Z])\w*\s')
+# Does exclude "here www.Berkeley.edu fuond"?
+# TODO Add year (first_published), author's names
+# TODO Include two words with . urls only if they have href
+# one dot, spaces exclude if not href
 
 import csv
 import sys
 import re
+import time
 
 from collections import OrderedDict
 
+from bs4 import BeautifulSoup
 from openpyxl import Workbook
 from openpyxl.styles import Alignment
 from tools import strip_tags, regex_url_pattern
@@ -41,18 +49,14 @@ regex_wiley = re.compile(r'''http://(api.)?onlinelibrary.wiley.com''',
 # Assume that URLs matches which are two words joint by '.' with first two
 # letters in mixed case are actually end and start of a sentence.
 # Exclude these.
-regex_sentence_url = re.compile(r'\s\w+\.([A-Z][a-z]|[a-z][A-Z])')
+regex_sentence_url = re.compile(r'^\w+\.([A-Z][a-z]|[a-z][A-Z])\w*$')
 
 # Define search terms other than URLs to check for explicit reference to data
 # or code. Order increasingly by false positive likelihood.
 # Match all indicators only at beginning of word.
-# Use "r'string'" to stop Python from interpreting \b as backspace.
-repref_indicators = [r'\b' + x for x in
-                     [r'codes?\b', r'replicat', r'repositor', r'dataverse\b',
-                      r'archives?', r'data\b', r'availab', r'found\b',
-                      r'authors?\b', r'websites?\b', r'homepages?\b',
-                      r'webpages?\b']
-                     ]
+repref_indicators = ['code[^d]', 'replicat', 'reposito', 'dataverse',
+                     'archive', 'data', 'availab', r'\bfound\b',
+                     'website', 'homepage', 'webpage']
 
 regex_repref_indicators = re.compile('(?:' + '|'.join(repref_indicators) + ')',
                                      re.IGNORECASE)
@@ -64,12 +68,12 @@ char_match_post = char_match_pre
 csv.field_size_limit(sys.maxsize)
 
 input_file = 'bld/ajps_articles_2003_2016.csv'
-output_file = 'bld/ajps_article_links.xlsx'
+output_file = 'bld/ajps_reference_coding_template.xlsx'
 
 # Initialize sheet.
 wb = Workbook()
 ws = wb.active
-ws.title = 'ajps_repref_coding'
+ws.title = 'ajps_reference_coding'
 
 # Format columns to make list of URLs easier for humans to read.
 for column in ['B', 'D']:
@@ -96,6 +100,12 @@ with open(input_file) as fh_in:
 
         content_html = article[ix_dict['content']]
 
+        content_soup = BeautifulSoup(content_html, 'lxml')
+        urls_from_href = [link['href'] for link in content_soup.find_all('a')]
+        # Exclude email and internal links, e.g. to footnotes.
+        urls_from_href = [u for u in urls_from_href
+                          if not u.startswith(('#', 'mailto:'))]
+
         # Remove HTML tags for easier extraction of url context.
         content_text = strip_tags(content_html)
 
@@ -104,10 +114,17 @@ with open(input_file) as fh_in:
                                  (regex_repref_indicators, content_text))
 
         # Exclude URLs following set of exceptions.
+        urls_from_href = [url for url in urls_from_href
+                          if regex_wiley.search(url) is None]
         urls = [url for url in urls if (regex_wiley.search(url[0]) is None
                                         and
                                         regex_sentence_url.search(url[0])
                                         is None)]
+
+        urls_from_regex = [u[0] for u in urls]
+        for u in urls_from_href:
+            if u not in urls_from_regex:
+                urls.append((u, 'HREF'))
 
         title_cell = ('=HYPERLINK("' + article_url(article[ix_dict['doi']])
                       + '","' + article[ix_dict['title']] + '")')
