530ce38628d86796ad8ca12237b213e56968d4c7
djbirke <djbirke@berkeley.edu>
2016-10-10 17:48:33 -0700

    Check for consistent merging, find conflicting entries

--- data_preparation/create_ajps_reference_coding_diff.py
+++ data_preparation/create_ajps_reference_coding_diff.py
@@ -3,15 +3,15 @@
 Create spreadsheet of differences in data entry.
 """
 
-# TODO Look into unmatched entries.
-
+import warnings
 from collections import OrderedDict
 
 from pyexcel_ods3 import get_data
-import numpy as np
 import pandas as pd
+import numpy as np
+from tools import fill_columns_down
 
-urap_initials = ['KJK', 'rk', 'RP', 'TC']
+urap_initials = ['KJK', 'RK', 'RP', 'TC']
 input_files = OrderedDict(zip(urap_initials,
                               [('data_entry/ajps_reference_coding_' +
                                 initials + '.ods')
@@ -25,28 +25,49 @@ for person, input_file in input_files.items():
     content = sheet[1:]
     entry = pd.DataFrame(columns=header, data=content)
 
-    # Add article info to every row.
-    for column in ['article_ix', 'doi', 'title']:
-        entry[column] = entry[column].replace('', np.nan)
-        entry[column].fillna(method='ffill', inplace=True)
+    fill_columns_down(entry, ['article_ix', 'doi', 'title'])
 
     # Add identifier to entry column.
     entry.rename(columns={'reference_category':
                           'reference_category_' + person},
                  inplace=True)
+
     entries[person] = entry
 
-columns_merge_on = [c for c in header if c != 'reference_category']
-merged_entries = pd.merge(entries['KJK'], entries['rk'],
-                          how='outer', on=columns_merge_on,
-                          indicator='KJK_with_rk')
+columns_merge_on = ['doi', 'article_ix', 'title', 'match', 'context']
+merged_entries = pd.merge(left=entries['KJK'], right=entries['RK'],
+                          suffixes=('_KJK', '_RK'), how='outer',
+                          on=columns_merge_on, left_index=True,
+                          right_index=True, indicator='KJK_with_RK')
 
-merged_entries = pd.merge(merged_entries, entries['RP'],
-                          how='outer', on=columns_merge_on,
-                          indicator='KJK_rk_with_TC')
+merged_entries = pd.merge(left=merged_entries, right=entries['RP'],
+                          suffixes=('_KJK_RK', '_RP'), how='outer',
+                          on=columns_merge_on, left_index=True,
+                          right_index=True, indicator='KJK_RK_with_RP')
 
-merged_entries = pd.merge(merged_entries, entries['TC'],
-                          how='outer', on=columns_merge_on,
-                          indicator='KJK_rk_RP_with_TC')
+merged_entries = pd.merge(left=merged_entries, right=entries['TC'],
+                          suffixes=('_KJK_RK_RP', '_TC'), how='outer',
+                          on=columns_merge_on, left_index=True,
+                          right_index=True, indicator='KJK_RK_RP_with_TC')
 
-merged_entries.to_csv(output_file, index_label='row_ix')
+consistent_template = np.all(merged_entries[['KJK_with_RK', 'KJK_RK_with_RP',
+                                             'KJK_RK_RP_with_TC']] == 'both',
+                             axis=(0, 1))
+
+if consistent_template:
+    reference_columns = ['reference_category_' + x for x in urap_initials]
+    merged_entries = merged_entries[columns_merge_on + reference_columns]
+
+    data_entered = merged_entries[reference_columns].notnull()
+    merged_entries['number_of_entries'] = np.sum(data_entered, axis=1)
+    merged_entries['conflict'] = merged_entries[reference_columns]\
+        [data_entered].apply(pd.Series.nunique, axis=1) > 1
+    bool_printing = {True: 'True', False: ''}
+    merged_entries.replace({'conflict': bool_printing}, inplace=True)
+
+    merged_entries.to_csv(output_file, index=None)
+
+else:
+    warnings.warn('''Some lines could not be matched consistently across files.
+                  Please investigate output file.''')
+    merged_entries.to_csv(output_file, index_label='row_ix')
