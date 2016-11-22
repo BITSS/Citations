452382136e8c087f9348846028ead740bdbc0eb9
djbirke <djbirke@berkeley.edu>
2016-11-21 17:11:17 -0800

    Add AJPS link coding diff resolution

--- data_preparation/create_diff_resolution_template.py
+++ data_preparation/create_diff_resolution_template.py
@@ -25,36 +25,24 @@ def add_resolution_columns(df, entry_columns, conflict_column,
 
     Otherwise leave blank for manual resolution.
     '''
-    left_column = entry_columns[0]
-    right_column = entry_columns[1]
-
     data_entered = df[entry_columns].notnull()
-    conflict = (df[entry_columns][data_entered].
-                replace('skip', value=np.nan).
-                apply(pd.Series.nunique, axis=1) > 1)
+    number_of_entries_unique = (df[entry_columns][data_entered].
+                                replace('skip', value=np.nan).
+                                apply(pd.Series.nunique, axis=1))
+    conflict = number_of_entries_unique > 1
 
-    left_entry = df[left_column].notnull()
-    right_entry = df[right_column].notnull()
+    skipped = df[entry_columns] == 'skip'
 
-    left_only = np.all([left_entry, ~right_entry], axis=0)
-    right_only = np.all([~left_entry, right_entry], axis=0)
+    for entry_column in entry_columns:
+        take_it = np.all([data_entered[entry_column], ~skipped[entry_column],
+                          number_of_entries_unique == 1], axis=0)
+        df.loc[take_it, resolution_column] = df.loc[take_it, entry_column]
 
-    left_skip = df[left_column] == 'skip'
-    right_skip = df[right_column] == 'skip'
-
-    agreement = np.equal(df[left_column], df[right_column])
-
-    take_left = np.any([left_only, agreement,
-                        np.all([left_skip, ~right_skip], axis=0)],
-                       axis=0)
-    take_right = np.any([right_only,
-                         np.all([right_skip, ~left_skip], axis=0)],
-                        axis=0)
+    agree_skip = np.all([np.any(skipped, axis=1),
+                         number_of_entries_unique == 0], axis=0)
+    df.loc[agree_skip, resolution_column] = 'skip'
 
     df[conflict_column] = conflict
-    df.loc[take_left, resolution_column] = df.loc[take_left, left_column]
-    df.loc[take_right, resolution_column] = df.loc[take_right, right_column]
-
 
 # Resolve AJPS reference coding diffs.
 resolution_pairs = [('KJK', 'RP'), ('RK', 'TC')]
@@ -67,15 +55,15 @@ for pair in resolution_pairs:
 
     suffix = '_' + '_'.join(pair)
 
-    reference_columns = ['reference_category_' + x for x in pair]
+    entry_columns = ['reference_category_' + x for x in pair]
     resolution_column = 'reference_category' + suffix + '_resolved'
     conflict_column = 'conflict_ignore_skip' + suffix
 
     output_columns = (['doi', 'article_ix', 'title', 'match', 'context'] +
-                      reference_columns + [resolution_column,
-                                           conflict_column])
+                      entry_columns + [resolution_column,
+                                       conflict_column])
 
-    add_resolution_columns(diff, entry_columns=reference_columns,
+    add_resolution_columns(diff, entry_columns=entry_columns,
                            conflict_column=conflict_column,
                            resolution_column=resolution_column)
 
@@ -97,17 +85,15 @@ for pair in resolution_pairs:
 
     suffix = '_' + '_'.join(pair)
 
-    reference_columns = ['website_category_' + x for x in pair]
+    entry_columns = ['website_category_' + x for x in pair]
     resolution_column = 'website_category' + suffix + '_resolved'
     conflict_column = 'conflict_ignore_skip' + suffix
 
-    article_columns = ['doi', 'article_ix', 'title']
-    match_columns = ['match', 'context']
     output_columns = (['article_ix', 'doi', 'title', 'author'] +
-                      reference_columns + [resolution_column,
-                                           conflict_column])
+                      entry_columns + [resolution_column,
+                                       conflict_column])
 
-    add_resolution_columns(diff, entry_columns=reference_columns,
+    add_resolution_columns(diff, entry_columns=entry_columns,
                            conflict_column=conflict_column,
                            resolution_column=resolution_column)
 
@@ -129,17 +115,15 @@ for pair in resolution_pairs:
 
     suffix = '_' + '_'.join(pair)
 
-    reference_columns = ['website_category_' + x for x in pair]
+    entry_columns = ['website_category_' + x for x in pair]
     resolution_column = 'website_category' + suffix + '_resolved'
     conflict_column = 'conflict_ignore_skip' + suffix
 
-    article_columns = ['doi', 'article_ix', 'title']
-    match_columns = ['match', 'context']
     output_columns = (['article_ix', 'doi', 'title', 'author'] +
-                      reference_columns + [resolution_column,
-                                           conflict_column])
+                      entry_columns + [resolution_column,
+                                       conflict_column])
 
-    add_resolution_columns(diff, entry_columns=reference_columns,
+    add_resolution_columns(diff, entry_columns=entry_columns,
                            conflict_column=conflict_column,
                            resolution_column=resolution_column)
 
@@ -148,3 +132,25 @@ for pair in resolution_pairs:
 
     diff.to_csv(output_file_prefix + suffix + '.csv',
                 columns=output_columns, index=None)
+
+
+# Resolve AJPS link coding diffs.
+persons = ['KJK', 'RP', 'RK', 'TC']
+
+input_file = 'bld/ajps_link_coding_diff.csv'
+output_file = 'bld/ajps_link_coding_diff_resolution.csv'
+
+entry_columns = ['link_category_' + x for x in persons]
+resolution_column = 'link_category_resolved'
+conflict_column = 'conflict_ignore_skip'
+output_columns = (['article_ix', 'doi', 'title', 'match', 'context'] +
+                  entry_columns + [resolution_column, conflict_column])
+diff = pd.read_csv(input_file)
+
+add_resolution_columns(diff, entry_columns=entry_columns,
+                       conflict_column=conflict_column, resolution_column=resolution_column)
+
+bool_printing = {True: 'True', False: ''}
+diff.replace({conflict_column: bool_printing}, inplace=True)
+
+diff.to_csv(output_file, columns=output_columns, index=None)
