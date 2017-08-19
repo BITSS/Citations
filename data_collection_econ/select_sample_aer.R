1cbe15c3ebb135a1149e8e175f5136daa73c5312
djbirke <djbirke@berkeley.edu>
2017-08-19 16:19:53 +0200

    Use AER file without article content to decrease output file size

--- data_collection_econ/select_sample_aer.R
+++ data_collection_econ/select_sample_aer.R
@@ -4,7 +4,7 @@ library(digest)
 set.seed(42)
 sample_rate = 0.5
 
-aer <- read_csv('../data_collection_econ/pdf_to_txt/complete_aer.csv',
+aer <- read_csv('data_collection_econ/aer.csv',
                 col_types = cols(publication_date = col_date(format = '%Y/%m')))
 
 aer <- aer %>%
@@ -13,7 +13,7 @@ aer <- aer %>%
   group_by(journal, publication_date) %>%
   arrange(journal, publication_date, hashed_doi) %>%
   mutate(hash_rank = 1:n(),
-         selected_into_sample = hash_rank <= ceiling(0.5 * n()))
+         selected_into_sample = hash_rank <= ceiling(sample_rate * n()))
 
-aer %>% write_csv('../data_collection_econ/aer_with_sample_selection.csv')
+aer %>% write_csv('data_collection_econ/aer_with_sample_selection.csv')
 
