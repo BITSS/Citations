689a18f26c7e84ec7b838c9f816e7d3bc41dc426
muyangshin <myang752@gmail.com>
2017-11-18 22:47:43 -0800

    Add reading from AJPS/APSR

--- citation_scraping/elsevier/get_elsevier_citation_number.py
+++ citation_scraping/elsevier/get_elsevier_citation_number.py
@@ -4,6 +4,7 @@ import aiofiles
 import json
 import sys
 import re
+import pathlib
 import numpy as np
 import pandas as pd
 
@@ -28,7 +29,7 @@ async def main_search(search_type):
         elif search_type == 'title':
             # Search by title
             for small_chunk in list(chunks(df[df.citation.isnull()], 100)):
-                tasks = [search_by_title(session, journal_name, row.title, row.publication_date.year) for row in small_chunk.itertuples()]
+                tasks = [search_by_title(session, journal_name, row.title, row.__getattribute__(date_varname).year) for row in small_chunk.itertuples()]
 
                 await asyncio.gather(*tasks)
                 await asyncio.sleep(1)  # Just in case
@@ -101,29 +102,47 @@ async def search_by_title(session, srctitle, title, pubyear):
 
 
 if __name__ == '__main__':
+    # region ARGUMENT --------------------------------------------------
     journal_names = {
         'aer': 'American Economic Review',
         'qje': 'Quarterly Journal of Economics',
         'apsr': 'American Political Science Review',
+        # 'apsr_centennial': 'American Political Science Review',
         'ajps': 'American Journal of Political Science',
     }
 
     file_names = {
         'aer': 'indexed_aer',
         'qje': 'indexed_qje',
-        'aspr': '',
-        'ajps': '',
+        'apsr': 'apsr_article_info_from_issue_toc',
+        # 'apsr_centennial': 'apsr_centennial_article_coding_template',
+        'ajps': 'ajps_article_info_from_issue_toc',
     }
 
     # journal name
     try:
         journal = sys.argv[1].lower()
         journal_name = journal_names[journal]
+
+        # AER and QJE use publication_date, APSR and AJPS uses issue_date
+        if journal in ['aer', 'qje']:
+            date_varname = 'publication_date'
+        elif journal in ['apsr', 'apsr_centennial', 'ajps']:
+            date_varname = 'issue_date'
+        else:
+            raise KeyError
     except IndexError:
         print("Please provide a journal name")
         raise
+    except KeyError:
+        print(f"{journal} is not in the list")
+        raise
 
-    # api key
+    pathlib.Path(f'citation_scraping/elsevier/jsons/{journal}/doi').mkdir(parents=True, exist_ok=True)
+    pathlib.Path(f'citation_scraping/elsevier/jsons/{journal}/title').mkdir(parents=True, exist_ok=True)
+    # endregion
+
+    # region API KEY
     try:
         with open('citation_scraping/elsevier/api_key.txt', 'r', encoding='utf-8') as file:
             api_key = file.readline()
@@ -133,21 +152,41 @@ if __name__ == '__main__':
 
     if api_key == "":
         sys.exit("No api key in api_key.txt")
+    # endregion
 
+    # region READ AND CLEAN ------------------------------------------------------------------
     # list of articles
     dois = []
     try:
         df = pd.read_csv(f'bld/{file_names[journal]}.csv', encoding="ISO-8859-1")
-        df = df.head(n=10)  # for testing
     except IOError:
         print(f"No bld/{file_names[journal]}.csv file")
         raise
 
-    # create wanted columns
-    df['publication_date'] = pd.to_datetime(df['publication_date'])
+    # publication_date / issue_date
+    if journal == 'apsr_centennial':
+        # APSR CENTENNIAL: needs issue_date column
+        df['issue_date'] = pd.to_datetime('2006-11-01')
+    else:
+        df[date_varname] = pd.to_datetime(df[date_varname])
+
     df['citation'] = np.nan
     df['result'] = 'not found'
 
+    # APSR and AJPS: filter by year
+    if journal in ['apsr', 'ajps']:
+        df = df[(df[date_varname] >= '2006-01-01') & (df[date_varname] < '2015-01-01')]
+
+    # AJPS: remove (pages ...)
+    if journal == 'ajps':
+        pattern_title = re.compile(' \(pages .*\)$')
+        df['title'] = df['title'].apply(lambda x: re.sub(pattern_title, '', x))
+
+    # for testing
+    # df = df.head(n=10)
+    # endregion
+
+    # region SEARCH -------------------------------------------------------------------------
     # run loop
     print(f"Searching {journal_name} articles on Scopus...")
     loop = asyncio.get_event_loop()
@@ -157,7 +196,7 @@ if __name__ == '__main__':
     print(f"Searching by DOI: DONE")
 
     # write to file: just to be safe
-    df.to_csv(f'bld/{journal}_citations_scopus.csv')
+    df.to_csv(f'bld/{journal}_citations_scopus.csv', encoding='utf-8')
 
     # search by title
     pattern_remove = re.compile('[?“”]')  # characters to remove
@@ -168,4 +207,5 @@ if __name__ == '__main__':
     print("Searching by title: DONE")
 
     # write to file
-    df.to_csv(f'bld/{journal}_citations_scopus.csv')
+    df.to_csv(f'bld/{journal}_citations_scopus.csv', encoding='utf-8')
+    # endregion
